ede0997d96b024c3de422948e0bf115db7a75ad7
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-14 12:51:07 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program finds the first digit of a number
+ * @author Thomas Quigley
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+  // finds and tells the user the first digit of their number
+  public static void firstDigit(int userNumber) {
+    // create the first digit variable
+    int beginningDigit = userNumber;
+
+    // a loop that stops when the beginning digit is a single digit
+    while (beginningDigit >= 10) {
+      // divide the beginning digit by 10 to remove the right most digit
+      beginningDigit = beginningDigit / 10;
+    } 
+    // tell the user the first digit
+    System.out.println("The first digit of " + userNumber + " is " + beginningDigit);
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // get the users number
+    System.out.println("What number would you like the first digit of?");
+    int userNumber = input.nextInt();
+
+    // call upon firstDigit
+    firstDigit(userNumber);
   }
 }
